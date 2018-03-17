1269cc5e350a067d7261c77ffd5c25530c6baf07
bac5899 <bac5899@g.rit.edu>
2018-03-17 16:03:35 -0400

    Added in decoding, taking a break, coming back to this

--- unpackme.py
+++ unpackme.py
@@ -1,6 +1,16 @@
+import base64
 import json
 from pprint import pprint
 
+
+def createJSON():
+    return "hell0:"
+
+"""
+Takes in a converted base64 string and returns:
+signed int, unsigned int, short, float, double, and another double in big endian. 
+In that order. Separated by a '_'. Uses 4 bytes ints in 32 bits
+"""
 def manipulate(data):
     print("inside manipulate function")
     print(data)
@@ -10,9 +20,14 @@ def main():
     try:     
         data = json.load(open('data.json'))
         pprint(data) #printing the data
-        #reading out the values
-        bytesInformation = data["bytes"] 
-        print(bytesInformation)#printing out the bytes info
+        bytesInformation = data["bytes"] #reading out the values
+        print("\nBefore Conversion: ${}".format(bytesInformation))#printing out the bytes info
+        
+        #now we need to convert from base64 to human readable language
+        convertedBase64 = base64.b64decode(bytesInformation)
+        print("After Conversion: ${}".format(convertedBase64))
+        
+        #calling manipulate, which will convert into the following way
         manipulate(bytesInformation)
     except:
         print("Error, data not loaded properly")
