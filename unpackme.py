1bd3e563fc4d848cac5e30ad85f7c550db534eb6
bac5899 <bac5899@g.rit.edu>
2018-03-18 18:00:49 -0400

    Added in some tests

--- unpackme.py
+++ unpackme.py
@@ -15,22 +15,33 @@ def manipulate(data):
     print("inside manipulate function")
     print(data)
 
+"""
+An encoded set of bytes are stored in a JSON file.
+the main method decodes the base64,
+unpacks the values from it, and then sends them back.
+We're using 4 byte ints int the context of a 32-bit
+platform. The next step is extracting those numbers
+from the byte string. And them sending them
+back to the solution endpoint.
+"""
 def main():
     print("importing data....")
     try:     
         data = json.load(open('data.json'))
         pprint(data) #printing the data
         bytesInformation = data["bytes"] #reading out the values
-        print("\nBefore Conversion: ${}".format(bytesInformation))#printing out the bytes info
-        
+        print("\nBefore Conversion: {}".format(bytesInformation))#printing out the bytes info
         #now we need to convert from base64 to human readable language
+        #convertedBase64 = base64.b64decode(bytesInformation)
         convertedBase64 = base64.b64decode(bytesInformation)
-        print("After Conversion: ${}".format(convertedBase64))
-        
+        print("After Conversion: {}".format(convertedBase64))
+        print("Secondary Conversion Test: {}".format(convertedBase64.decode("utf-8")))
         #calling manipulate, which will convert into the following way
         manipulate(bytesInformation)
     except:
         print("Error, data not loaded properly")
 
-
+    #this is an experiment, to make sure the conversion is working properly
+    print("\nExperiment: {} ".format(base64.b64decode("TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCAuLi4=")))
+    print("ExperimentV2: {}".format(base64.b85decode("uz6hiDLQ0eCLKwAA9C5uQiUVvDscizBAQDCLHDu8FSU=")))
 if __name__ == "__main__": main()
